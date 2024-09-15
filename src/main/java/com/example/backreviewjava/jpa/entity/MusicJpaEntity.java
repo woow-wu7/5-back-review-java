176dc3192d4b232399ff8453b7733e7959d7e762
SPIYYN <spiyyn@cathaypacific.com>
2024-09-15 10:31:23 +0800

    ffeat: jpa.

--- src/main/java/com/example/backreviewjava/jpa/entity/MusicJpaEntity.java
+++ src/main/java/com/example/backreviewjava/jpa/entity/MusicJpaEntity.java
@@ -14,6 +14,23 @@ import java.util.Date;
 // - 2. 学习 ( swagger3 ) ( lombok 注解 ) 请到 ( beam/MusicTestBean )
 
 
+// JPA
+// 1. @Entity
+// 2. @Table("music")
+//
+// 3. @Id
+// 3. @GeneratedValue(strategy = GenerationType.IDENTITY)
+//
+// 4. @Column(name = "name", nullable = false, unique = true, length = 512)
+// name: The 'name' represents the field that is mapped to the database.
+// nullable: The 'nullable=false' means that the 'name' is not null.
+// unique: The 'unique=true' field means this field is unique in the database.
+// length: The 'length=512' field means the max length of this field is 512.
+// TIPS: the 'music_name' field correspond to the 'name' field in the database.
+// -- map: 地图n 映射v
+// -- correspond: 对应 相当于 类似于
+// -- 【 correspond to. 和...相对应 】
+
 //@Data
 //@Builder
 //@AllArgsConstructor
@@ -27,10 +44,21 @@ import java.util.Date;
 public class MusicJpaEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Column(name = "id", nullable = false)
+    // @JsonInclude(JsonInclude.Include.NON_NULL)
     public Integer id;
 
-    public String name;
+    // name
+    // name: The 'name' represents the field that is mapped to the database.
+    // nullable: The 'nullable=false' means that the 'name' is not null.
+    // unique: The 'unique=true' field means this field is unique in the database.
+    // length: The 'length=512' field means the max length of this field is 512.
+    // TIPS: the 'music_name' field correspond to the 'name' field in the database.
+    // -- map: 地图n 映射v
+    // -- correspond: 对应 相当于 类似于
+    // -- 【 correspond to. 和...相对应 】
+    @Column(name = "name", nullable = false, unique = true, length = 512)
+    public String music_name;
 
     // @JsonIgnore // ignore this field
     public String album;
