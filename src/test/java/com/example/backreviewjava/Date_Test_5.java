46aa4d35354481121e25d99ed8e3c3e3740816b7
SPIYYN <spiyyn@cathaypacific.com>
2024-10-20 15:47:37 +0800

    feat: date2.

--- src/test/java/com/example/backreviewjava/Date_Test_5.java
+++ src/test/java/com/example/backreviewjava/Date_Test_5.java
@@ -62,5 +62,68 @@ public class Date_Test_5 {
         OffsetDateTime offsetDateTime2 = parsedDateTime.atStartOfDay(ZoneId.of("UTC")).toOffsetDateTime();
         log.warn("offsetDateTime: {}", offsetDateTime);
         log.warn("offsetDateTime2: {}", offsetDateTime2);
+
+    }
+
+
+    @Test
+    public void calculate() {
+
+        // 1
+        LocalDate now = LocalDate.now();
+        LocalDate oneYearAgo = now.minusYears(1);
+        LocalDate oneDayAgo = now.minusDays(1);
+        log.warn("oneYearAgo: {}", oneYearAgo);
+        log.warn("oneDayAgo: {}", oneDayAgo);
+
+
+        // 2.1
+        // Starting time of a date
+        LocalDateTime atStartOfDay = now.atStartOfDay();
+        LocalDateTime atStartOfDay2= now.atTime(LocalTime.MIN);
+        log.warn("atStartOfDay: {}", atStartOfDay);
+        log.warn("atStartOfDay2: {}", atStartOfDay2); // The two functions are equal to each other.
+
+        // 2.2
+        // LocalDateTime -> ZonedDateTime -> OffsetDateTime
+        // -- 1. LocalDateTime 不包含时区信息，因此无法直接表示一个具体的时间点
+        // -- 2. ZonedDateTime 包含时区信息，可以表示一个具体的时间点
+        // -- 3. OffsetDateTime 包含时区偏移量，也可以表示一个具体的时间点，但它不包含完整的时区规则（例如夏令时）
+        OffsetDateTime offsetDateTime = atStartOfDay.atZone(ZoneOffset.ofHours(8)).toOffsetDateTime();
+
+
+        // 3
+        // ending time of a date
+        LocalDateTime atEndOfDay = now.atTime(LocalTime.MAX);
+        log.warn("atEndOfDay: {}", atEndOfDay);
+
+
+        // 4
+        // format
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        // 4.1
+        // localDate -> string
+        // localDateTime -> string
+        // localDate.format / localDateTime.format
+        LocalDate localDate = LocalDate.of(2023, 10, 1);
+        LocalDateTime localDateTime = LocalDateTime.of(2023, 10, 1, 12, 0, 0);
+        String formattedDateTimeString = localDate.format(formatter);
+        String formattedDateTimeString2 = localDateTime.format(formatter2);
+        log.warn("formattedDateTimeString: {}", formattedDateTimeString);
+        log.warn("formattedDateTimeString2: {}", formattedDateTimeString2);
+
+
+        // 4.2
+        // string -> LocalDate
+        // string -> LocalDateTime
+        // LocalDate.parse / LocalDateTime.parse
+        String dateString = "2022-01-01";
+        String datetimeString = "2022-01-01 12:00:00";
+        LocalDate parsedDateTime = LocalDate.parse(dateString, formatter);
+        LocalDateTime parsedDateTime2 = LocalDateTime.parse(datetimeString, formatter2);
+        log.warn("parsedDateTime: {}", parsedDateTime);
+        log.warn("parsedDateTime2: {}", parsedDateTime2);
     }
 }
